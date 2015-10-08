7a7f6ea314cccdf9261b5ff4557f7a8c7fae3abd
marb <marb61a@gmail.com>
2015-10-08 13:29:08 +0000

    added creating exercise spec

---
+++ spec/features/creating_exercise_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.feature "Creating Exercise" do
+    before do
+        @john = User.create(first_name: "Joe",
+        last_name: "Bloggs",
+        email: "joe@example.com", 
+        password: "password")
+        login_as(@john)
+    end
+    
+    sceanrio "with valid inputs" do
+        visit "/"
+        
+        click_link "My Lounge"
+        click_link "New Workout"
+        expect(page).to have_link("Back")
+        
+        fill_in "Duration", with: 70
+        fill_in "Workout Details", with: "Weight Lifting"
+        fill_in "Activity date", with: Date.today
+        
+        click_button "Create Exercise"
+        
+        expect (page).to have_content("Exercise has been created")
+        
+        exercise = Exercise.last
+        
+        expect(page.current_path).to eq(user_exercise_path(@joe, exercise))
+    end
+    
+    scenario "with invalid inputs" do
+        visit "/"
+        
+        click_link "My Lounge"
+        click_link "New Workout"
+        expect(page).to have_link("Back")
+        
+        fill_in "Duration", with: nil
+        fill_in "Workout Details", with: ""
+        fill_in "Activity date", with: ""
+        click_button "Create Exercise"
+        
+        expect(page).to have_content("Exercise has not been created")
+        
+        expect(page).to have_content("Duration in min can't be blank")
+        expect(page).to have_content("Duration in min is not a number")
+        expect(page).to have_content("Workout can't be blank")
+        expect(page).to have_content("Workout date can't be blank")
+    end
+end
