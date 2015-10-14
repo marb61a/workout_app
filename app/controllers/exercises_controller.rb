08fb4e7dc254934455c3aacddcbb152e8fa667a3
marb <marb61a@gmail.com>
2015-10-14 13:24:00 +0000

    updated exercises controller and exercises show view

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,7 @@
 class ExercisesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_exercise, except: [:index, :new, :create]
+    
     def index
         
     end
@@ -8,11 +11,40 @@ class ExercisesController < ApplicationController
     end
     
     def create
-        
+        @exercise = current_user.exercises.new(exercise_params)
+        if @exercise.save
+          flash[:success] = "Exercise has been created"
+          redirect_to [current_user, @exercise]
+        else
+          flash[:danger] = "Exercise has not been created"
+          render :new
+        end
     end
     
     def edit
     
     end
+    
+    def update
+        
+    end
+    
+    def show
+        
+    end
+    
+    def destroy
+        
+    end
+    
+    private
+  
+      def exercise_params
+        params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+      end
+      
+      def set_exercise
+        @exercise = current_user.exercises.find(params[:id])
+      end
     
 end
