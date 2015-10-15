4decb7ccfc9dbf89998ed8b5b8778d798919673c
marb <marb61a@gmail.com>
2015-10-15 13:17:46 +0000

    added exercises index table

--- spec/features/listing_exercises_spec.rb
+++ spec/features/listing_exercises_spec.rb
@@ -8,4 +8,16 @@ RSpec.feature "Listing Exercises" do
         @e1 = @joe.exercises.create(duration_in_min: 20, workout: "Body building routine", workout_date: Date.today)
         @e2 = @joe.exercises.create(duration_in_min: 20, workout: "Cardio", workout_date: Date.today)
     end
+    
+    scenario "Show user's workouts for the last 7 days" do
+        visit "/"
+    
+        click_link "My Lounge"
+        expect(page).to have_content(@e1.duration_in_min)
+        expect(page).to have_content(@e1.workout)
+        expect(page).to have_content(@e1.workout_date)
+        expect(page).to have_content(@e2.duration_in_min)
+        expect(page).to have_content(@e2.workout)
+        expect(page).to have_content(@e2.workout_date)
+    end
 end
