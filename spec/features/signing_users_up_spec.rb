93ee8272635e4c059f929e131df85a0bd0b23215
marb <marb61a@gmail.com>
2015-10-27 14:10:40 +0000

    updated user signup feature test

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -3,16 +3,32 @@ require "rails_helper"
 RSpec.feature "Users signup" do
     scenario "with valid credentials" do
         visit "/"
-        
         click_link "Sign up"
-
+        fill_in "First name", with: "Joe"
+        fill_in "Last name", with: "Bloggs"
         fill_in "Email", with: "user@example.com"
         fill_in "Password", with: "password"
         fill_in "Password confirmation", with: "password"
-
         click_button "Sign up"
-
-        expect(page).to have_content("You have signed up successfully.")
         
+        expect(page).to have_content("You have signed up successfully.")
+        visit "/"
+        expect(page).to have_content("Joe Bloggs")
+        
+    end
+    
+    scenario "with invalid credentials" do
+        visit "/"
+        
+        click_link "Sign up"
+        fill_in "First name", with: ""
+        fill_in "Last name", with: ""
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Sign up"
+        
+        expect(page).to have_content("First name can't be blank")
+        expect(page).to have_content("Last name can't be blank")
     end
 end
