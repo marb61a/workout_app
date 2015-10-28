af9ebeb09febb3442b6bc90720b9f11a993627ea
marb <marb61a@gmail.com>
2015-10-28 13:45:36 +0000

    added friendship view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations'}
   get 'dashboard/index'
   
   resources :users do
     resources :exercises
   end
+  
+  resource :dashboard, only: [:index] do
+    collection do
+      post :search, to: 'dashboard#search'
+    end
+  end
+  
+  resources :friendships, only: [:show, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
