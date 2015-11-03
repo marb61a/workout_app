87aecc2453eda92ea2895ac98c8ceb429b8238c9
marb <marb61a@gmail.com>
2015-11-03 09:02:46 +0000

    updated controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,7 +8,7 @@ class ExercisesController < ApplicationController
     end
     
     def new
-        @exercise = current_user.exercises.new(exercise_params)
+        @exercise = current_user.exercises.new
     end
     
     def create
